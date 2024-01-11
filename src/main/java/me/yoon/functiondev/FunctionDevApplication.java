b067cce52c817ecaf0b9e00a24c98e609b5b25df
buchonsi <buchonsi@naver.com>
2024-01-11 17:33:35 +0900

    init project

---
+++ src/main/java/me/yoon/functiondev/FunctionDevApplication.java
@@ -0,0 +1,13 @@
+package me.yoon.functiondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yoon/functiondev/FunctionDevApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yoon.functiondev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
